d50db1c0c5f948a4ce69f5d52191f79ba2832c11
Yasunori Mahata <mahata777@gmail.com>
2015-01-29 17:25:28 -0800

    Add peek

--- pstone/lexer.py
+++ pstone/lexer.py
@@ -5,7 +5,8 @@ from pstone.token import Token
 
 class Lexer(object):
     def __init__(self):
-        self.hasMore = True
+        # self.hasMore = True
+        self.queue = []
         self.lines = open("/etc/resolv.conf").read().split("\n")  # ToDo: Fix Me
 
     def read(self):
@@ -13,5 +14,12 @@ class Lexer(object):
             ret = self.line[0]
             self.line = self.line[1:]
             return ret
+        else:
+            return Token(-1)  # EOF
+
+    def peek(self):
+        pass
+        if (0 < len(self.line)):
+            return self.line[0]
         else:
             return Token(-1)  # EOF
