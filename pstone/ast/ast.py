a666c7ba48d72a9cf115f3a898eda6abd003760c
Yasunori Mahata <mahata777@gmail.com>
2015-02-04 21:06:41 -0800

    Add an abstract ASTree class

--- pstone/ast/ast.py
+++ pstone/ast/ast.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+from abc import ABCMeta
+
 
 class ASTree(object):
+    __metaclass__ = ABCMeta
+
+    def numChildren(self):
+        raise Exception("Abstract method has been called.")
+
+    def children(self):
+        raise Exception("Abstract method has been called.")
+
+    def location(self):
+        raise Exception("Abstract method has been called.")
+
+    def iterator(self):
+        raise Exception("Abstract method has been called.")
+
+
+class ASTList(ASTree):
+    pass
+
+
+class ASTLeaf(ASTree):
     pass
