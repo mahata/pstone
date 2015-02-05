458a503766a8400d6a1a3e14691779fa18cef75e
Yasunori Mahata <mahata777@gmail.com>
2015-02-04 21:26:05 -0800

    Add ASTLeaf class

--- pstone/ast/ast.py
+++ pstone/ast/ast.py
@@ -24,4 +24,24 @@ class ASTList(ASTree):
 
 
 class ASTLeaf(ASTree):
-    pass
+
+    def __init__(self, token):
+        self.token = token
+
+    def child(self):
+        raise Exception("ToDo: Fix Me - something like IndexOutOfBoundsException")
+
+    def numChildren(self):
+        return 0
+
+    def children(self):
+        return []  # ToDo: ??
+
+    def location(self):
+        return "at line %d" % self.token.getLineNumber()
+
+    def token(self):
+        return self.token
+
+    def __str__(self):
+        return self.token.getText()
