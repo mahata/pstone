1210331d25b674e33426ff81864bb58c806bcd6b
Yasunori Mahata <mahata777@gmail.com>
2015-02-22 20:53:28 -0800

    Minor

---
+++ snippets/parsing/recursive-descent/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from token import Token
+from lexer import Lexer
+from listlexer import ListLexer
+from sys import argv
+
+if __name__ == "__main__":
+    lexer = ListLexer(argv[1])
+    token = lexer.next_token()
+    while token.kind is not Lexer.EOF_KIND:
+        print(token)
+        token = lexer.next_token()
+    print(token)  # EOF
