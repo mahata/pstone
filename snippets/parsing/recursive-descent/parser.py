4c89143a819fab9c204b14d2f2b31e6363e63fe4
Yasunori Mahata <mahata777@gmail.com>
2015-02-22 21:26:34 -0800

    Minor

---
+++ snippets/parsing/recursive-descent/parser.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Parser(metaclass=ABCMeta):
+    def __init__(self, input):
+        self.input = input
+        self.consume()  # set self.lookahead
+
+    def match(x):
+        if self.lookahead.kind == x:
+            self.consume()
+        else:
+            raise Exception("exception %s; %s" % (self.input.get_token_name(x), self.lookahead))
+
+    def consume(self):
+        self.lookahead = self.input.next_token()
